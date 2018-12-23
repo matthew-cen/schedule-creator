ae33dab6bc78b24bc4cef4e5c12ae397b25af6e3
matthew-cen <mc6844@nyu.edu>
2018-12-23 15:00:34 -0500

    Strip whitespace from user input

--- utilities.py
+++ utilities.py
@@ -1,6 +1,6 @@
 VALID_DAYS = {"SUNDAY" : 0, "MONDAY": 1, "TUESDAY": 2, "WEDNESDAY": 3, "THURSDAY": 4, "FRIDAY": 5, "SATURDAY": 6}
 def parse_day(day_str):
-    return VALID_DAYS[day_str.upper()]
+    return VALID_DAYS[day_str.strip().upper()]
 
 def parse_time(time_int): # parses time passed as integer as minutes since 00:00
     parsed_int = int(time_int)
