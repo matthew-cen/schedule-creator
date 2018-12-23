5ab0f47b258e4cf322e20cb02745543d83eb9c28
matthew-cen (HeliosLHC) <mc6844@nyu.edu>
2018-12-22 23:07:56 -0500

    Implemented __str__ method for class Section

--- classes.py
+++ classes.py
@@ -2,9 +2,12 @@
 # TODO: Generate schedule inferface
 
 class Section:
-    def __init__(self, time_start_end, days_lst):
+    def __init__(self, section_id,time_start_end, days_lst):
+        self.section_id = section_id
         self.timeslot = time_start_end
-        self.days = days_lst
+        self.days = days_lst #TODO: change to use binary string?
+    def __str__(self):
+        return f"Section ID {self.section_id} Time: {self.timeslot} Days: {self.days}"    
 class Course:
     # FIELDS
     sections = {}
@@ -13,6 +16,7 @@ class Course:
     def __init__(self, course_id, course_name):
         self.course_id = course_id
         self.course_name = course_name
+
     # COMMAND METHODs
     def add_section(self):        
         section_id = input("Please enter the section ID: ")
@@ -32,17 +36,20 @@ class Course:
                     break
                 elif user_res != "Y":
                     print("[ERROR] Invalid command. Please try again") 
-            self.sections[section_id] = Section((time_start, time_end), section_days)
+            self.sections[section_id] = Section(section_id, (time_start, time_end), section_days)
+
     def modify_section(self, section_id):
         if self.section_exists(section_id):
             pass
         else:
             print(f"The following section does not exist: {section_id}")
+
     def remove_section(self, section_id):
         if self.section_exists(section_id):
             self.sections.pop(section_id)
         else:
             print(f"The following section does not exist: {section_id}")
+
     # INTERFACE
     def interface(self):
         while True:
@@ -62,12 +69,15 @@ class Course:
                     self.remove_section(user_selected_course)
             except ValueError:
                 print("[ERROR] Invalid command. Please enter a number between 1 and 3")
+
     # UTILITY METHODS
     def __repr__(self):
         print("Section ID: ", self.course_id, "\n")
         print("----------------------------------------")
+
     def __str__(self):
         return f"Course #: {self.course_id} Course Name: {self.course_name}"
+
     @staticmethod
     def print_commands(self):
         print("1) Add a new section")
@@ -88,6 +98,7 @@ class Course:
         else:
             print("<<<You have added no sections to this course>>>")
         print("-----------------------------------------------------")
+
     def section_exists(self, section_id):
         return section_id in self.sections.keys()
 
@@ -97,6 +108,7 @@ class Database:
     """
     # FIELDS
     courses = {}
+
     # COMMAND METHODS
     def add_course(self):
         """
@@ -109,6 +121,7 @@ class Database:
         else: 
             self.courses[course_id] = Course(course_id, course_name) # instantiate new Course object
             print(f"Successfully added the following course: {course_name}")
+
     def modify_course(self,course_id):
         """
         Command Number : 2
@@ -132,6 +145,7 @@ class Database:
     @staticmethod
     def gen_schedules(self):
         pass
+        
     # INTERFACE METHOD
     def interface(self):
         while True:
@@ -159,6 +173,7 @@ class Database:
         print("2) Modify a course")
         print("3) Remove a course")
         print("4) Generate Valid Schedules")
+
     @staticmethod
     def print_courses(self):
         print(f"Number of Courses: {len(self.courses)}")
@@ -172,5 +187,6 @@ class Database:
         else:
             print("<<<You have added no courses>>>")
         print("-----------------------------------------------------")
+
     def course_exists(self, course_id):
         return course_id in self.courses.keys()
