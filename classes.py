7e1d6a33f60b5d1f8bd3f6d48209e16a7d0396bb
matthew-cen (HeliosLHC) <mc6844@nyu.edu>
2018-12-22 20:59:37 -0500

    Reorganized Code

---
+++ classes.py
@@ -0,0 +1,132 @@
+class Section:
+    timeslots = []
+    def __init__(self, section_id):
+        self.section_id = section_id                                                            
+    def add_time_slot(self, timeLow, timeHigh): 
+        self.timeslots.append((timeLow, timeHigh))
+
+class Course:
+    # FIELDS
+    sections = {}
+    
+    # METHODS
+    def __init__(self, course_num, course_name):
+        self.course_num = course_num
+        self.course_name = course_name
+    # COMMAND METHODs
+    def add_section(self):        
+        # self.sections.append(section) 
+        pass
+    def modify_section(self, section_id):
+        pass
+    def remove_section(self, section_id):
+        pass
+    # INTERFACE
+    def interface(self):
+        self.print_sections(self) # show sections in current course
+        self.print_commands(self)
+    # UTILITY METHODS
+    def __repr__(self):
+        print("Section ID: ", self.course_num, "\n")
+        print("----------------------------------------")
+    def __str__(self):
+        return f"Course #: {self.course_num} Course Name: {self.course_name}"
+    @staticmethod
+    def print_commands(self):
+        print("1) Add a new section")
+        print("2) Modify a section")
+        print("3) Remove a section")
+    @staticmethod
+    def print_sections(self):
+        print(f"Number of Sections: {len(self.sections)}")
+        print("-----------------------------------------------------")
+        # Check if course list is empty
+        if len(self.sections):
+            counter = 1
+            for section_id in self.sections:
+                print(f"{counter} - {self.sections[section_id]}")
+                counter += 1
+        else:
+            print("<<<You have added no sections to this course>>>")
+        print("-----------------------------------------------------")
+    @staticmethod
+    def section_exists(self, section_id):
+        return section_id in self.sections.keys()
+
+class Database:
+    """
+    pseudo-database stored as dictionary
+    """
+    # FIELDS
+    courses = {}
+    # COMMAND METHODS
+    def add_course(self):
+        """
+        Command Number : 1
+        """
+        course_num = input("Please enter the course number: ") # asks user to input course num 
+        course_name = input("Please enter the course name: ") # asks user to input course name
+        if course_num in self.courses.keys(): # checks if the course number already exists
+            print(f"There is already a course with this course number with name: {self.courses[course_num]}")
+        else: 
+            self.courses[course_num] = Course(course_num, course_name) # instantiate new Course object
+            print(f"Successfully added the following course: {course_name}")
+    def modify_course(self,course_num):
+        """
+        Command Number : 2
+        """
+        print(f"Selected the following course for modification: {course_num}")
+        pass
+
+    def remove_course(self, course_num):
+        """
+        Command Number : 3
+        """
+        try:
+            self.courses.pop(course_num) # remove course from database
+            print(f"Successfully removed the following course: {course_num}")
+        except KeyError:
+            print("[ERROR] The provided course number does not exist:" + course_num)
+    # INTERFACE METHOD
+    def interface(self):
+        while True:
+            print() # prints newline for readability 
+            self.print_courses(self) # show user added courses
+            self.print_commands(self) # show user available commands
+            try:
+                user_res = int(input("Enter a command via the command number: "))
+                # Validate input as a number 
+                if user_res == 1:
+                    self.add_course()
+                elif user_res == 2:
+                    user_selected_course = input("Please enter the course number you want to modify: ")
+                    self.modify_course(user_selected_course)
+                elif user_res == 3:
+                    user_selected_course = input("Please enter the course number you want to remove: ")
+                    self.remove_course(user_selected_course)
+            except ValueError:
+                print("[ERROR] Invalid command. Please enter a number between 1 and 3")
+
+    # UTILITY METHODS
+    @staticmethod # function decorator for a static method (python's version of const) 
+    def print_commands(self):
+        print("1) Add a new course")
+        print("2) Modify a course")
+        print("3) Remove a course")
+    @staticmethod
+    def print_courses(self):
+        print(f"Number of Courses: {len(self.courses)}")
+        print("-----------------------------------------------------")
+        # Check if course list is empty
+        if len(self.courses):
+            counter = 1
+            for course_num in self.courses:
+                print(f"{counter} - {self.courses[course_num]}")
+                counter += 1
+        else:
+            print("<<<You have added no courses>>>")
+        print("-----------------------------------------------------")
+
+    @staticmethod
+    def course_exists(self, course_num):
+        return course_num in self.courses.keys()
--- interface.py
+++ interface.py
@@ -1,91 +1,9 @@
 # TODO: Implement Exceptions
 
 # IMPORTS
-from datetime import *
+from classes import Database
 # CONSTANTS
 VALID_DAY_LST = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
-
-# UTILITIES
-
-# CLASSES
-class Section:
-    timeslots = []
-    def __init__(self, section_id):
-        self.section_id = section_id                                                            
-    def add_time_slot(self, timeLow, timeHigh): # Chris add time slot 6:19pm
-        timeslots.append((timeLow, timeHigh))
-
-class Course:
-    # FIELDS
-    sections = []
-    
-    # METHODS
-    def __init__(self, course_num, course_name):
-        self.course_num = course_num
-        self.course_name = course_name
-    # def add_section
-
-    def add_section(self, section): # Chris add section 6:19
-        # i don't think i'm adding the section thing correctly... (we gotta declare type)? 
-        self.sections.append(section) 
-    # def add_timeslot
-
-    def __repr__(self):
-        print("Course Name: ", self.course_name, "\n")
-        print("Course Number: ", self.course_num, "\n")
-        print("----------------------------------------")
-
-class Database:
-    """
-    pseudo-database stored as dictionary
-
-    """
-    # FIELDS
-    courses = {}
-    # METHODS
-    def add_course(self):
-        course_num = input("Please enter the course number: ") # asks user to input course num 
-        course_name = input("Please enter the course name: ") # asks user to input course name
-        self.courses[course_num] = Course(course_num, course_name) # instantiate new Course object
-    
-    def modify_course(self,course_num):
-        pass
-    def print_courses(self):
-        print("Number of Courses: ", end="")
-        # Check if course list is empty
-        if len(self.courses):
-            print(len(self.courses))
-            counter = 1
-            for course in self.courses:
-                print(counter + ")" + self.courses[course])
-        else:
-            print("0 \nYou have added no courses")
-    def remove_course(self, course_num):
-        try:
-            courses.pop(course_num) # remove course from database
-        except KeyError:
-            print("[ERROR] The provided course number does not exist:" + course_num)
-    def interface(self):
-        self.print_commands() # show user available commands
-        while true:
-            user_res = input("Enter a command via the command number: ")
-            
-            # Validate input as a number 
-            if user_res == 1:
-                self.add_course()
-            elif user_res == 2:
-                self.modify_course()
-            elif user_res == 3:
-                self.remove_course()
-            else:
-                print("Invalid command. Please enter a number between 1 and 3")
-
-
-    @staticmethod # function decorator for a static method (python's version of const) 
-    def print_commands(self):
-        print("1) Add a new course")
-        print("2) Modify a course")
-        print("3) Remove a course")
 
 
 # MAIN
@@ -93,35 +11,33 @@ def main():
     # initialize new database
     database = Database()
 
-    # Output current courses
-    database.print_courses()
+    # Initialize main user interface
+    database.interface()
 
+    # # Main User Interface Loop
+    # while True: # true and false need to capitalize first letter
+    #     print("0 - Monday, 1 - Tuesday, 2 - Wednesday, 3 - Thursday, 4 - Friday, 5 - Saturday, 6 - Sunday")
     
-    # Main User Interface Loop
-    while True: # true and false need to capitalize first letter
-        print("0 - Monday, 1 - Tuesday, 2 - Wednesday, 3 - Thursday, 4 - Friday, 5 - Saturday, 6 - Sunday")
-    
-        res_day = input("Please select a day using the number or the day name : ")
-        if res_day.lower() not in VALID_DAY_LST or res_day.isdigit(): 
-            # if the input was invalid            
-            print("Invalid input, please try again - You provided: ", res_day)
-        else: 
-            # if the input was valid                                                           
-            while True:
-                res_timeslot = input("Please enter the start and end times for timeslot in 24HR format (ex. 03:30) :")
-                # input format is like "0600 2300" initially (beta version)                                
-                # res_timeslot is string input
-                # have to exit from while loop, no break (next line code unreachable on pycharm)
-                if (0 < int(res_timeslot[0:3]) < 2400 and 0 < int(res_timeslot[5:8]) <2400):
-                    newClass = Course(course_name, course_num)
-                    # checks if number is between 0 and 2400
-                    # need to store the time slot into something
-                    break                
-            if not input("Add more timeslots? (Y/N):"):
-                break
-            if not input("Add more days?"):
-                res_repeat = input("Do you want to copy over existing timeslots from previous days? (Y/N): ")
-            if
-            break
+    #     res_day = input("Please select a day using the number or the day name : ")
+    #     if res_day.lower() not in VALID_DAY_LST or res_day.isdigit(): 
+    #         # if the input was invalid            
+    #         print("Invalid input, please try again - You provided: ", res_day)
+    #     else: 
+    #         # if the input was valid                                                           
+    #         while True:
+    #             res_timeslot = input("Please enter the start and end times for timeslot in 24HR format (ex. 03:30) :")
+    #             # input format is like "0600 2300" initially (beta version)                                
+    #             # res_timeslot is string input
+    #             # have to exit from while loop, no break (next line code unreachable on pycharm)
+    #             if (0 < int(res_timeslot[0:3]) < 2400 and 0 < int(res_timeslot[5:8]) <2400):
+    #                 newClass = Course(course_name, course_num)
+    #                 # checks if number is between 0 and 2400
+    #                 # need to store the time slot into something
+    #                 break                
+    #         if not input("Add more timeslots? (Y/N):"):
+    #             break
+    #         if not input("Add more days?"):
+    #             res_repeat = input("Do you want to copy over existing timeslots from previous days? (Y/N): ")
+    #         break
 
 main()
