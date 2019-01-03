f129fb2509f6f9a225b72883c88d9e15be7a8b4b
matthew-cen <mc6844@nyu.edu>
2019-01-03 17:53:46 -0500

    Add course operation function outlines

--- static/main.js
+++ static/main.js
@@ -1,3 +1,28 @@
 function toggleModal() {
-    document.querySelector(".modal").classList.toggle("is-active")
+  document.querySelector(".modal").classList.toggle("is-active");
+}
+// add new course
+function addCourse() {
+  // SEND DATA TO SERVER FOR VALIDATION
+  // STORE COURSE DATA IN SESSIONSTORAGE
+  // RENDER NEW COURSE COMPONENT
+}
+// remove existing course
+function removeCourse() {
+  // SEND DATA TO SERVER FOR VALIDATION
+  // REMOVE COURSE DATA IN SESSIONSTORAGE
+  // NRENDER COURSE COMPONENT
+}
+// update course information
+function updateCourse() {
+  // SEND DATA TO SERVER FOR VALIDATION
+  // UPDATE COURSE DATA IN SESSIONSTORAGE
+  // RERENDER COURSE COMPONENT
+}
+
+// retrieve course data from server and store it in browser
+function getCourse() {
+  // SEND DATA TO SERVER FOR VALIDATION
+  // STORE COURSE DATA IN SESSIONSTORAGE
+  // RENDER NEW COURSE COMPONENT
 }
