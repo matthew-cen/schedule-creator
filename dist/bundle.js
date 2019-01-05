21265b2ffe2204ece3425372320603ffd97b7736
matthew-cen <mc6844@nyu.edu>
2019-01-04 21:19:24 -0500

    Reorganize code

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,100 @@
-!function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t){(()=>{let e=document.querySelector("form");function t(){document.querySelector(".modal").classList.toggle("is-active")}document.addEventListener("DOMContentLoaded",()=>{document.getElementById("addCourseBtn").addEventListener("click",t),document.querySelector("#createCourseModal header button").addEventListener("click",t),e.addEventListener("submit",t=>{t.preventDefault();new FormData(e);fetch("/api/hello").then(e=>{console.log(e)}).catch(e=>{console.error("An error occurred")})})})})()}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./js-src/main.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./js-src/main.js":
+/*!************************!*\
+  !*** ./js-src/main.js ***!
+  \************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("(() => {\n\t// PSEUDO GLOBALS\n\n\n\t// use DOMContentLoaded\n\tdocument.addEventListener(\"DOMContentLoaded\", () => {\n\t\tconst addCourseForm = document.getElementById(\"addCourseForm\");\n\t\tconst courseList = document.getElementById(\"courseList\");\n\t\tfunction toggleModal() {\n\t\t\tdocument.querySelector(\".modal\").classList.toggle(\"is-active\");\n\t\t}\n\t\t\n\t\t// Event Listeners\n\t\t// Modal Toggling\n\t\tdocument\n\t\t\t.getElementById(\"addCourseBtn\")\n\t\t\t.addEventListener(\"click\", toggleModal);\n\t\tdocument\n\t\t\t.querySelector(\"#createCourseModal header button\")\n\t\t\t.addEventListener(\"click\", toggleModal);\n\n\t\t// Custom AJAX handling for form submission\n\n\t\t// Form Submit Button\n\t\taddCourseForm.addEventListener(\"submit\", event => {\n\t\t\tevent.preventDefault(); // disable default page refresh behavior on submit\n\t\t\taddCourse();\n\t\t\tlet addCourseFormData = new FormData(addCourseForm); // create FormData from form element\n\t\t\t// fetch(\"/api/hello\")\n\t\t\t// \t.then(res => {\n\t\t\t// \t\tconsole.log(res);\n\t\t\t// \t})\n\t\t\t// \t.catch(err => {\n\t\t\t// \t\tconsole.error(\"An error occurred\");\n\t\t\t// \t});\n\t\t});\n\n\t\t// add new course\n\t\tfunction addCourse() {\n\t\t\t// SEND DATA TO SERVER FOR VALIDATION\n\t\t\t// STORE COURSE DATA IN SESSIONSTORAGE\n\t\t\t// RENDER NEW COURSE COMPONENT\n\t\t\tcourseList.insertAdjacentHTML(\"beforeend\");\n\t\t\tconst newCourseComponent = `\n\t\t\t<div class=\"notification is-primary is-12 box\">\n\t\t\t<button class=\"delete\"></button>\n\t\t\t<div class=\"columns\">\n\t\t\t<div class=\"column is-11\">\n\t\t\t<p class=\"title\">CS-UY 2134 - Object\n\t\t\tOriented\n\t\t\tProgramming</p>\n\t\t\t</div>\n\t\t\t<div class=\"column is-1\">\n\t\t\t<button class=\"button is-white\">\n\t\t\t<span class=\"icon\">\n\t\t\t<i class=\"fas fa-pencil-alt fa-lg\"></i>\n\t\t\t</span>\n\t\t\t<span>\n\t\t\tEdit\n\t\t\t</span>\n\t\t\t</button>\n\t\t\t</div>\n\t\t\t</div>\n\t\t\t</div>`;\n\t\t}\n\t\t// remove existing course\n\t\tfunction removeCourse() {\n\t\t\t// SEND DATA TO SERVER FOR VALIDATION\n\t\t\t// REMOVE COURSE DATA IN SESSIONSTORAGE\n\t\t\t// NRENDER COURSE COMPONENT\n\t\t}\n\t\t// update course information\n\t\tfunction updateCourse() {\n\t\t\t// SEND DATA TO SERVER FOR VALIDATION\n\t\t\t// UPDATE COURSE DATA IN SESSIONSTORAGE\n\t\t\t// RERENDER COURSE COMPONENT\n\t\t}\n\n\t\t// retrieve course data from server and store it in browser\n\t\tfunction getCourse() {\n\t\t\t// SEND DATA TO SERVER FOR VALIDATION\n\t\t\t// STORE COURSE DATA IN SESSIONSTORAGE\n\t\t\t// RENDER NEW COURSE COMPONENT\n\t\t}\n\t});\n})();\n\n\n//# sourceURL=webpack:///./js-src/main.js?");
+
+/***/ })
+
+/******/ });
--- js-src/main.js
+++ js-src/main.js
@@ -1,13 +1,14 @@
 (() => {
-	// PSEUDO GLOBALS
-	const addCourseForm = document.getElementById("addCourseForm");
-	const courseList = document.getElementById("courseList");
-	function toggleModal() {
-		document.querySelector(".modal").classList.toggle("is-active");
-	}
-	// FIXME: Fix onload behavior to trigger when the DOM tree or certain elements are loaded
-	// use DOMContentLoaded
 	document.addEventListener("DOMContentLoaded", () => {
+		const addCourseForm = document.getElementById("addCourseForm");
+		const courseList = document.getElementById("courseList");
+		const createCourseModal = document.getElementById("createCourseModal");
+
+		// UTILITY FUNCTIONS
+		function toggleModal() {
+			createCourseModal.classList.toggle("is-active");
+		}
+
 		// Event Listeners
 		// Modal Toggling
 		document
@@ -19,65 +20,66 @@
 
 		// Custom AJAX handling for form submission
 
-		// Override Form Submit Behavior
+		// Form Submit Button
 		addCourseForm.addEventListener("submit", event => {
 			event.preventDefault(); // disable default page refresh behavior on submit
+			addCourse();
 			let addCourseFormData = new FormData(addCourseForm); // create FormData from form element
-			fetch("/api/hello")
-				.then(res => {
-					console.log(res);
-				})
-				.catch(err => {
-					console.error("An error occurred");
-				});
+			// fetch("/api/hello")
+			// 	.then(res => {
+			// 		console.log(res);
+			// 	})
+			// 	.catch(err => {
+			// 		console.error("An error occurred");
+			// 	});
 		});
+
+		// add new course
+		function addCourse() {
+			// SEND DATA TO SERVER FOR VALIDATION
+			// STORE COURSE DATA IN SESSIONSTORAGE
+			// RENDER NEW COURSE COMPONENT
+			courseList.insertAdjacentHTML("beforeend");
+			const newCourseComponent = `
+			<div class="notification is-primary is-12 box">
+			<button class="delete"></button>
+			<div class="columns">
+			<div class="column is-11">
+			<p class="title">CS-UY 2134 - Object
+			Oriented
+			Programming</p>
+			</div>
+			<div class="column is-1">
+			<button class="button is-white">
+			<span class="icon">
+			<i class="fas fa-pencil-alt fa-lg"></i>
+			</span>
+			<span>
+			Edit
+			</span>
+			</button>
+			</div>
+			</div>
+			</div>`;
+		}
+		// remove existing course
+		function removeCourse() {
+			// SEND DATA TO SERVER FOR VALIDATION
+			// REMOVE COURSE DATA IN SESSIONSTORAGE
+			// NRENDER COURSE COMPONENT
+		}
+		// update course information
+		function updateCourse() {
+			// SEND DATA TO SERVER FOR VALIDATION
+			// UPDATE COURSE DATA IN SESSIONSTORAGE
+			// RERENDER COURSE COMPONENT
+		}
+
+		// retrieve course data from server and store it in browser
+		function getCourse() {
+			// SEND DATA TO SERVER FOR VALIDATION
+			// STORE COURSE DATA IN SESSIONSTORAGE
+			// RENDER NEW COURSE COMPONENT
+		}
 	});
-
-	// add new course
-	function addCourse() {
-		// SEND DATA TO SERVER FOR VALIDATION
-		// STORE COURSE DATA IN SESSIONSTORAGE
-		// RENDER NEW COURSE COMPONENT
-		let test = `
-		<div class="notification is-primary is-12 box">
-		<button class="delete"></button>
-		<div class="columns">
-		<div class="column is-11">
-		<p class="title">CS-UY 2134 - Object
-		Oriented
-		Programming</p>
-		</div>
-		<div class="column is-1">
-		<button class="button is-white">
-		<span class="icon">
-		<i class="fas fa-pencil-alt fa-lg"></i>
-		</span>
-		<span>
-		Edit
-		</span>
-		</button>
-		</div>
-		</div>
-		</div>
-		`;
-	}
-	// remove existing course
-	function removeCourse() {
-		// SEND DATA TO SERVER FOR VALIDATION
-		// REMOVE COURSE DATA IN SESSIONSTORAGE
-		// NRENDER COURSE COMPONENT
-	}
-	// update course information
-	function updateCourse() {
-		// SEND DATA TO SERVER FOR VALIDATION
-		// UPDATE COURSE DATA IN SESSIONSTORAGE
-		// RERENDER COURSE COMPONENT
-	}
-
-	// retrieve course data from server and store it in browser
-	function getCourse() {
-		// SEND DATA TO SERVER FOR VALIDATION
-		// STORE COURSE DATA IN SESSIONSTORAGE
-		// RENDER NEW COURSE COMPONENT
-	}
 })();
